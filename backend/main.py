67fceb3d7ed3a66565f3a79b0e9776003f48b25a
yanavyklyuk <yana.vykliuk.shi.2023@lpnu.ua>
2025-12-16 15:50:25 +0200

    Added patient_router and changed main

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from backend.config import engine, Base
+from backend.routers import patient_router
 
-app = FastAPI(
-    title="Smaragd MVP"
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Smaragd Clinic API")
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+app.include_router(patient_router.router)
 
 @app.get("/")
-def healthcheck():
-    return {"status": "ok"}
+def root():
+    return {"message": "Smaragd API is running!"}
